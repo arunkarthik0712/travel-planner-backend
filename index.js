91b25a6de6c852d55c8db79b168b4e085a8bd1de
arunkarthik0712 <arunkarthik0710@gmail.com>
2024-08-20 22:09:11 +0530

    index

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "https://arunkarthik0710-travel-planner.netlify.app" }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
