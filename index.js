302e0bf377771cf7125ecf5391751493b10c49b8
Arun Karthik <154799238+arunkarthik0712@users.noreply.github.com>
2025-02-13 01:32:54 +0530

    add localhost HTTPS origin to allowedOrigins

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const allowedOrigins = [
   "https://arunkarthik0710-travel-planner.netlify.app", // Web (Netlify)
   "capacitor://localhost", // iOS App
   "http://localhost", // Android Emulator
+  "https://localhost", // Localhost (HTTPS)
   "http://192.168.1.100", // Mobile device (replace with your local IP)
   "http://com.travelplanner.app://localhost", // Android & iOS (real devices)
 ];
